e5c7f0b516ccb487f9b0bd95485d1d2ed079fccf
sir_Gollum <sir.gollum@gmail.com>
2022-12-18 11:31:42 +0100

    Add logging config

---
+++ aocutils/debug.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+import logging
+import logging.config
+
+
+def configure_logging(level, format="%(message)s"):
+    logging.config.dictConfig({
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "default": {
+                "format": format,
+            },
+        },
+        "handlers": {
+            "default": {
+                "level": level,
+                "formatter": "default",
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+            },
+        },
+        "loggers": {
+            "": {
+                "handlers": ["default"],
+                "level": level,
+            },
+        }
+    })
