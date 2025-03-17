5bfcd6ef8e34fa3be9cd23e0411e275a2523b11a
sir_Gollum <sir.gollum@gmail.com>
2025-03-17 21:36:52 +0100

    Add pattern finder

--- aocutils/iteration.py
+++ aocutils/iteration.py
@@ -1,11 +1,30 @@
 # coding: utf-8
+from collections import defaultdict
+import tqdm
 
 
 def chunkwise(iterable, chunk_size):
     for i in range(0, len(iterable), chunk_size):
-        yield iterable[i:i + chunk_size]
+        yield iterable[i : i + chunk_size]
 
 
 def sliding_window(iterable, window_size):
     for i in range(len(iterable) - window_size + 1):
-        yield iterable[i:i + window_size]
+        yield iterable[i : i + window_size]
+
+
+def find_repeating_patterns(lst, skip_length_below=2, show_progress=False):
+    patterns = defaultdict(list)
+    length = len(lst)
+
+    for start in tqdm.tqdm(range(length), disable=not show_progress):
+        for end in range(start + 1, length + 1):
+            if len(lst[start:end]) < skip_length_below:
+                continue
+            pattern = tuple(lst[start:end])
+            patterns[pattern].append(start)
+
+    repeated_patterns = {
+        pattern: indices for pattern, indices in patterns.items() if len(indices) > 1
+    }
+    return repeated_patterns
